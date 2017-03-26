8a87d24c067d20bac2d3605f6e36ecdd4a2f3830
David Marcella <davmarcella@gmail.com>
2017-03-26 17:32:55 -0400

    adding JUNIT test

---
+++ davidsTest.java
@@ -0,0 +1,26 @@
+import org.junit.Assert;
+
+import org.junit.Test;
+
+
+
+public class TestFailure {
+
+	
+	@Test
+	public void whenCharactersNotRecognizedBySqliteAreInString(){
+		String input = '"';
+		Assert.assertEquals(true, addToDatabase(input));
+	}
+	
+	
+	public static boolean addToDatabase(string input){
+		if(input.indexOf('"') >= 0){
+			input = input.replaceAll("\"|\"", "");
+		}
+		
+		//adds to actual database
+		return true;	
+	}
+}
+
